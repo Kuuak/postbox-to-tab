525d9464797ba04bb3c5b0a6c7d0361d402dc0f5
Felipe Paul Martins <fpm@opusmagnum.ch>
2015-11-21 12:51:13 +0100

    initial commit with ready plugin

---
+++ postbox-to-tab.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Plugin Name:				Postbox to Tab
+ * Description: 			Convert the sortable post boxes into a tabbed menu.
+ * Author: 						Felipe Paul Martins - Opus Magnum
+ * Version: 					1.0
+ * Author URI:				http://opusmagnum.ch
+ * License:     			GPL-2.0+
+ * License URI: 			http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @package   postbox-to-tab
+ * @author    Felipe Paul Martins <fpmpusmagnum.ch>
+ * @license   GPL-2.0+
+ * @link      http://opusmagnum.ch
+ */
+
+/* Prevent loading this file directly */
+defined( 'ABSPATH' ) || exit;
+
+if ( !class_exists( 'Postbox_to_tab' ) ) {
+
+	/**
+   * Class Postbox_to_tab
+   * @since 1.0
+   */
+	class Postbox_to_tab {
+
+		/**
+		 * Class Constructor.
+		 * @since  1.0
+		 */
+		public function __construct() {
+			add_action( 'plugins_loaded', array( $this, 'postbox_to_tab_setup' ), 1 );
+			add_action( 'plugins_loaded', array( $this, 'postbox_to_tab_init' ), 10 );
+		}
+
+		/**
+		 * Setup ID, Version, Directory path, and URI
+		 * @since  1.0
+		 */
+		public function postbox_to_tab_setup() {
+			$this->id = 'postbox_to_tab';
+			$this->version = '1.0';
+			$this->directory_path = trailingslashit( plugin_dir_path( __FILE__ ) );
+			$this->directory_uri  = trailingslashit( plugin_dir_url(  __FILE__ ) );
+		}
+
+		/**
+		 * Init the plugin functions
+		 * @since  1.0
+		 */
+		public function postbox_to_tab_init() {
+
+			add_action('admin_enqueue_scripts', array( $this, 'postbox_to_tab_enqueue_files') );
+		}
+
+		/**
+		 * Enqueuing js/css files only in edition page
+		 * @since  1.0
+		 * @param string $hook Hook suffix for the current admin page.
+		 */
+		public function postbox_to_tab_enqueue_files( $hook ) {
+
+			if ( 'post.php' != $hook ) return;
+
+			wp_enqueue_script( 'postbox_to_tab_js', $this->directory_uri . 'js/postbox-to-tab.js', array('jquery'), $this->version );
+			wp_enqueue_style( 'postbox_to_tab_css', $this->directory_uri . 'css/postbox-to-tab.css', false, $this->version );
+		}
+	}
+}
+
+new Postbox_to_tab();
---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== Postbox to Tab ===
+Contributors: Kuuak
+Requires at least: 4.3.1
+Tested up to: 4.3.1
+Stable tag: 1.0
+License: GPL-2.0+
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+
+Convert the sortable post boxes into a tabbed menu.
+
+== Description ==
+Convert the sortable post boxes into a tabbed menu.
+
+Currently, only the postbox under the body are converted and they aren\'t sortable.
+
+= Todo =
+- A settings page
+- Select all or specific post/page/custom post type
+- Restore the sort/drag&drop to the postbox
+
+== Installation ==
+To install, simply:
+
+1. Upload the \'postbox-to-tab\' directory to your \'/wp-content/plugins/\' directory, using your favorite method (ftp, sftp, scp, etc...)
+2. Activate BuddyPress from your Plugins page.
+
+== Screenshots ==
+1. The postboxes under the body are displayed in tabs
+2. The postboxes under the body are displayed in tabs
+
+== Changelog ==
+= 1.0 =
+Initial version
