a5c4811b55a28fa390a8bd9cc13846a20ed5dc1f
Felipe <felipe.paulmartins@outlook.com>
2016-10-18 12:36:31 +0200

    Update and add Internationalization

--- postbox-to-tab.php
+++ postbox-to-tab.php
@@ -7,6 +7,8 @@
  * Author URI:				http://opusmagnum.ch
  * License:     			GPL-2.0+
  * License URI: 			http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:		pbtt
+ * Domain Path:		/languages
  *
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
  * General Public License version 2, as published by the Free Software Foundation.  You may NOT assume
